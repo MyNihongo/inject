8174a8101188f9dc479b0e7e448526513ed0b4c2
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-19 23:10:58 +0300

    test parse for inline

--- cmd/inject/mainLoad_test.go
+++ cmd/inject/mainLoad_test.go
@@ -14,18 +14,25 @@ func getFilePath(fileName string) string {
 	return path.Join("test", fmt.Sprintf("%s.txt", fileName))
 }
 
-func TestLoad(t *testing.T) {
-	ctx := context.Background()
-	wd, _ := os.Getwd()
-	filePath := getFilePath("line_split")
-
+func TestLoadLineSplit(t *testing.T) {
 	want := map[string]injectType{
 		"pkg1.GetService1": Singleton,
 		"pkg2.GetService2": Transient,
 	}
 
-	mapping, err := loadFileContent(ctx, wd, filePath)
+	ctx := context.Background()
+	wd, _ := os.Getwd()
 
-	assert.Nil(t, err)
-	assert.Equal(t, want, mapping)
+	fileNames := []string{
+		"line_split",
+		"inline",
+	}
+
+	for _, fileName := range fileNames {
+		filePath := getFilePath(fileName)
+		mapping, err := loadFileContent(ctx, wd, filePath)
+
+		assert.Nil(t, err)
+		assert.Equal(t, want, mapping)
+	}
 }
