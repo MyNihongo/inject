9acc206d15ae3064610b39f10d5cae6826276f4a
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-21 23:48:50 +0300

    generate code for one

--- cmd/inject/mainGenerate.go
+++ cmd/inject/mainGenerate.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/MyNihongo/codegen"
 )
 
 func generateServiceProvider(pkgName string, diGraph map[string]*pkgFuncs) (*codegen.File, error) {
 	file := codegen.NewFile(pkgName, "my-nihongo-di")
-	// imports := file.Imports()
+	imports := file.Imports()
 
-	// for pkgImport, funcs := range diGraph {
+	for pkgImport, pkgFuncs := range diGraph {
+		imports.AddImportAlias(pkgImport, pkgFuncs.alias)
 
-	// 	for _, v := range funcs {
+		for returnType, funcDecl := range pkgFuncs.funcs {
+			diFunc := file.Func(fmt.Sprintf("Provide%s", returnType)).ReturnTypes(
+				codegen.QualReturnType(pkgFuncs.alias, returnType),
+			)
 
-	// 	}
-	// }
+			if len(funcDecl.paramTypes) == 0 {
+				diFunc.Block(
+					codegen.Return(codegen.QualFuncCall(pkgFuncs.alias, funcDecl.name)),
+				)
+			} else {
+				// blocks := make([]codegen.Block, len(funcDecl.paramTypes))
+			}
+		}
+	}
 
 	return file, nil
 }
