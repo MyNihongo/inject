af8b94631aa505257f25b20871997d5e66977229
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-21 09:35:20 +0300

    continue on nil

--- cmd/inject/mainDefinition.go
+++ cmd/inject/mainDefinition.go
@@ -29,8 +29,9 @@ func getDefinitions(ctx context.Context, wd string, loaded *loadResult) error {
 						return fmt.Errorf("%s is not a function", injection.function)
 					} else if signature, ok := funcDecl.Type().(*types.Signature); !ok {
 						return fmt.Errorf("cannot retrieve a signature of %s", injection.function)
+					} else if params := signature.Params(); params == nil {
+						continue
 					} else {
-						params := signature.Params()
 						for i := 0; i < params.Len(); i++ {
 							param := params.At(i)
 							fmt.Println(param.Type())
