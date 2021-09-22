bcc6934aed16fc3ee077189ed441a02c2bf596a7
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-22 22:40:16 +0300

    singleton and transiaent - no params

--- cmd/inject/mainGenerate.go
+++ cmd/inject/mainGenerate.go
@@ -8,25 +8,60 @@ import (
 
 func generateServiceProvider(pkgName string, diGraph map[string]*pkgFuncs) (*codegen.File, error) {
 	file := codegen.NewFile(pkgName, "my-nihongo-di")
-	imports := file.Imports()
+	imports, isSyncAdded := file.Imports(), false
 
 	for pkgImport, pkgFuncs := range diGraph {
 		imports.AddImportAlias(pkgImport, pkgFuncs.alias)
 
 		for returnType, funcDecl := range pkgFuncs.funcs {
-			diFunc := file.Func(fmt.Sprintf("Provide%s", returnType)).ReturnTypes(
-				codegen.QualReturnType(pkgFuncs.alias, returnType),
-			)
+			var stmts []codegen.Stmt
+			if funcDecl.injectType == Singleton {
+				if isSyncAdded {
+					imports.AddImport("sync")
+				}
 
-			if len(funcDecl.paramTypes) == 0 {
-				diFunc.Block(
-					codegen.Return(codegen.QualFuncCall(pkgFuncs.alias, funcDecl.name)),
-				)
+				varName := fmt.Sprintf("impl_%s", returnType)
+				file.DeclareVars(codegen.QualVar(varName, pkgFuncs.alias, returnType))
+
+				stmts = generateInjectionStats(pkgFuncs, funcDecl, func(v codegen.Value) codegen.Stmt {
+					return codegen.Assign(varName).Values(v)
+				})
+
+				stmts = []codegen.Stmt{
+					codegen.QualFuncCall("sync", "DoOnce").Args(codegen.Lambda().Block(stmts...)),
+					codegen.Return(codegen.Identifier(varName)),
+				}
 			} else {
-				// blocks := make([]codegen.Block, len(funcDecl.paramTypes))
+				stmts = generateInjectionStats(pkgFuncs, funcDecl, func(v codegen.Value) codegen.Stmt {
+					return codegen.Return(v)
+				})
 			}
+
+			file.Func(fmt.Sprintf("Provide%s", returnType)).ReturnTypes(
+				codegen.QualReturnType(pkgFuncs.alias, returnType),
+			).Block(stmts...)
 		}
 	}
 
 	return file, nil
 }
+
+func generateInjectionStats(pkgFuncs *pkgFuncs, funcDecl *funcDecl, finalBlockFunc func(codegen.Value) codegen.Stmt) []codegen.Stmt {
+	provideFunc := codegen.QualFuncCall(pkgFuncs.alias, funcDecl.name)
+
+	if len(funcDecl.paramTypes) == 0 {
+		return []codegen.Stmt{
+			finalBlockFunc(provideFunc),
+		}
+	} else {
+		panic("aaa")
+	}
+}
+
+// if len(funcDecl.paramTypes) == 0 {
+// 	diFunc.Block(
+// 		codegen.Return(codegen.QualFuncCall(pkgFuncs.alias, funcDecl.name)),
+// 	)
+// } else {
+// 	// blocks := make([]codegen.Block, len(funcDecl.paramTypes))
+// }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/MyNihongo/inject
 go 1.17
 
 require (
-	github.com/MyNihongo/codegen v0.0.11
+	github.com/MyNihongo/codegen v0.0.12
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/tools v0.1.6
 )
