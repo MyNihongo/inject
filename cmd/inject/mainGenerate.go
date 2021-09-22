2d55e6a69c1482b0559981a31531256453eae053
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-22 23:54:55 +0300

    create a param defintion

--- cmd/inject/mainGenerate.go
+++ cmd/inject/mainGenerate.go
@@ -11,10 +11,10 @@ func generateServiceProvider(pkgName string, diGraph map[string]*pkgFuncs) (*cod
 	file := codegen.NewFile(pkgName, "my-nihongo-di")
 	imports, isSyncAdded := file.Imports(), false
 
-	for pkgImport, pkgFuncs := range diGraph {
-		imports.AddImportAlias(pkgImport, pkgFuncs.alias)
+	for pkgImport, pkgDecl := range diGraph {
+		imports.AddImportAlias(pkgImport, pkgDecl.alias)
 
-		for returnType, funcDecl := range pkgFuncs.funcs {
+		for returnType, funcDecl := range pkgDecl.funcs {
 			var err error
 			var stmts []codegen.Stmt
 			usedDecls := make(map[*typeDecl]string)
@@ -25,26 +25,34 @@ func generateServiceProvider(pkgName string, diGraph map[string]*pkgFuncs) (*cod
 				}
 
 				varName := fmt.Sprintf("impl_%s", returnType)
-				file.DeclareVars(codegen.QualVar(varName, pkgFuncs.alias, returnType))
+				file.DeclareVars(codegen.QualVar(varName, pkgDecl.alias, returnType))
 
-				stmts, err = createInjectionStmts(diGraph, pkgFuncs, funcDecl, func(v codegen.Value) codegen.Stmt {
+				stmts, err = createInjectionStmts(diGraph, pkgDecl, funcDecl, usedDecls, func(v codegen.Value) codegen.Stmt {
 					return codegen.Assign(varName).Values(v)
 				})
+
+				if err != nil {
+					return nil, err
+				}
 
 				stmts = []codegen.Stmt{
 					codegen.QualFuncCall("sync", "DoOnce").Args(codegen.Lambda().Block(stmts...)),
 					codegen.Return(codegen.Identifier(varName)),
 				}
 			} else {
-				stmts, err = createInjectionStmts(diGraph, pkgFuncs, funcDecl, func(v codegen.Value) codegen.Stmt {
+				stmts, err = createInjectionStmts(diGraph, pkgDecl, funcDecl, usedDecls, func(v codegen.Value) codegen.Stmt {
 					return codegen.Return(v)
 				})
+
+				if err != nil {
+					return nil, err
+				}
 			}
 
 			funcName, injectName := fmt.Sprintf("Provide%s", returnType), getInjectionName(funcDecl.injectType)
 			file.CommentF("%s provides a %s instance of %s.%s", funcName, injectName, pkgImport, returnType)
 			file.Func(funcName).ReturnTypes(
-				codegen.QualReturnType(pkgFuncs.alias, returnType),
+				codegen.QualReturnType(pkgDecl.alias, returnType),
 			).Block(stmts...)
 		}
 	}
@@ -52,7 +60,7 @@ func generateServiceProvider(pkgName string, diGraph map[string]*pkgFuncs) (*cod
 	return file, nil
 }
 
-func createInjectionStmts(diGraph map[string]*pkgFuncs, pkgFuncs *pkgFuncs, funcDecl *funcDecl, finalBlockFunc func(codegen.Value) codegen.Stmt) ([]codegen.Stmt, error) {
+func createInjectionStmts(diGraph map[string]*pkgFuncs, pkgFuncs *pkgFuncs, funcDecl *funcDecl, usedDecls map[*typeDecl]string, finalBlockFunc func(codegen.Value) codegen.Stmt) ([]codegen.Stmt, error) {
 	provideFunc := codegen.QualFuncCall(pkgFuncs.alias, funcDecl.name)
 
 	if len(funcDecl.paramDecls) == 0 {
@@ -61,16 +69,42 @@ func createInjectionStmts(diGraph map[string]*pkgFuncs, pkgFuncs *pkgFuncs, func
 		}, nil
 	} else {
 		var ok bool
-		for _, paramDecl := range funcDecl.paramDecls {
+		stmts := make([]codegen.Stmt, 0)
+		vals := make([]codegen.Value, len(funcDecl.paramDecls))
 
-			if pkgFuncs, ok = diGraph[paramDecl.pkgImport]; !ok {
+		for i, paramDecl := range funcDecl.paramDecls {
+			if usedParam, ok := usedDecls[paramDecl]; ok {
+				vals[i] = codegen.Identifier(usedParam)
+				continue
+			}
+
+			if nestedPkgFuncs, ok := diGraph[paramDecl.pkgImport]; !ok {
 				return nil, fmt.Errorf("package %s is not registered", paramDecl.pkgImport)
-			} else if funcDecl, ok = pkgFuncs.funcs[paramDecl.typeName]; !ok {
+			} else if nestedFuncDecl, ok := nestedPkgFuncs.funcs[paramDecl.typeName]; !ok {
 				return nil, fmt.Errorf("type %s is not found in the package %s", paramDecl.typeName, paramDecl.pkgImport)
 			} else {
+				// TODO: verify against the base (singleton cannot inject transient)
+				nestedFuncDecl.injectType
 
+				newParam := fmt.Sprintf("p%d", len(usedDecls))
+				vals[i] = codegen.Identifier(newParam)
+				usedDecls[paramDecl] = newParam
+
+				// TODO: assignment
+				a, err := createInjectionStmts(diGraph, nestedPkgFuncs, nestedFuncDecl, usedDecls, func(v codegen.Value) codegen.Stmt {
+
+				})
+
+				if err != nil {
+					return nil, err
+				} else {
+					stmts = append(stmts, a...)
+				}
 			}
 		}
+
+		stmts = append(stmts, provideFunc.Args(vals...))
+		return stmts, nil
 	}
 }
 
@@ -84,11 +118,3 @@ func getInjectionName(injectType injectType) string {
 		panic(fmt.Sprintf("unknown injection type: %d", injectType))
 	}
 }
-
-// if len(funcDecl.paramTypes) == 0 {
-// 	diFunc.Block(
-// 		codegen.Return(codegen.QualFuncCall(pkgFuncs.alias, funcDecl.name)),
-// 	)
-// } else {
-// 	// blocks := make([]codegen.Block, len(funcDecl.paramTypes))
-// }
